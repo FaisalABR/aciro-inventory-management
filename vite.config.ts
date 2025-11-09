53aab250dcd15774f55e88fbd3e8eb65c10bef99
FaisalABR <faisalabubakar92@gmail.com>
2025-11-09 17:07:00 +0700

    revert build

--- vite.config.ts
+++ vite.config.ts
@@ -19,13 +19,4 @@ export default defineConfig({
         "import.meta.env": {},
         "import.meta.glob": {},
     },
-    build: {
-        manifest: true,
-        outDir: "public/build",
-        rollupOptions: {
-            output: {
-                manualChunks: undefined, // ✅ Disable code splitting if issues persist
-            },
-        },
-    },
 });
